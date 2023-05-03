f485709c56d95b65634b1f55956e14337449a8db
gkfktkrh153 <gkfktkrh153@naver.com>
2023-05-03 13:52:11 +0900

    Test: 호감표시 또는 등록시 알림사항 등록 테스트 작성

--- src/test/java/com/ll/gramgram/boundedContext/notification/controller/NotificationControllerTest.java
+++ src/test/java/com/ll/gramgram/boundedContext/notification/controller/NotificationControllerTest.java
@@ -4,10 +4,12 @@ import com.ll.gramgram.base.rq.Rq;
 import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
 import com.ll.gramgram.boundedContext.likeablePerson.controller.LikeablePersonController;
 import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
 import com.ll.gramgram.boundedContext.member.entity.Member;
 import com.ll.gramgram.boundedContext.member.service.MemberService;
 import com.ll.gramgram.boundedContext.notification.entity.Notification;
 import com.ll.gramgram.boundedContext.notification.service.NotificationService;
+import com.ll.gramgram.standard.util.Ut;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
@@ -27,6 +29,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -56,6 +59,8 @@ class NotificationControllerTest {
     @Autowired
     private NotificationService notificationService;
 
+    @Autowired
+    private LikeablePersonService likeablePersonService;
     @Test
     @DisplayName("공지사항을 확인했을 때 readDate갱신")
     @WithUserDetails("user4")
@@ -80,8 +85,72 @@ class NotificationControllerTest {
 
         List<Notification> notifications = notificationService.findByToInstaMember(instaMember);
 
+        /*
+         공지사항 리스트를 확인하는 순간 나에 대한 공지사항들은 전부 readDate가 갱신됨
+         */
         notifications.stream()
                 .forEach(notification -> Assertions.assertThat(notification.getReadDate()).isNotNull());
+
+    }
+    @Test
+    @DisplayName("호감표시를 했을 때 Notification등록")
+    @WithUserDetails("user4")
+    void t002() throws Exception {
+
+
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/usr/likeablePerson/like")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("username", "insta_user2")
+                        .param("attractiveTypeCode", "1")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(LikeablePersonController.class))
+                .andExpect(handler().methodName("like"))
+                .andExpect(status().is3xxRedirection());
+
+        Member user = memberService.findByUsername("user2").orElse(null);
+        InstaMember instaMember = user.getInstaMember();
+
+        List<Notification> notifications = notificationService.findByToInstaMember(instaMember);
+
+        Assertions.assertThat(notifications.size()).isEqualTo(1);
+
+    }
+    @Test
+    @DisplayName("호감을 변경했을 때 Notification등록")
+    @WithUserDetails("user2")
+    void t003() throws Exception {
+
+
+        // coolTime갱신
+        LikeablePerson likeablePerson = likeablePersonService.findById(1L).orElse(null);
+        Ut.reflection.setFieldValue(likeablePerson, "modifyUnlockDate", LocalDateTime.now().plusSeconds(-1));
+
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(post("/usr/likeablePerson/modify/1")
+                        .with(csrf()) // CSRF 키 생성
+                        .param("attractiveTypeCode", "3")
+                )
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(LikeablePersonController.class))
+                .andExpect(handler().methodName("modify"))
+                .andExpect(status().is3xxRedirection());
+
+        Member user = memberService.findByUsername("user3").orElse(null);
+        InstaMember instaMember = user.getInstaMember();
+
+        List<Notification> notifications = notificationService.findByToInstaMember(instaMember);
+        Assertions.assertThat(notifications.size()).isEqualTo(2);
+        // notPr
 
     }
 
